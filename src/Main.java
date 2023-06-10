0a46509aaf65b01c7dc7a0150c270d79ca0658ea
Krisz6 <szalay.krisztian93@gmail.com>
2023-06-10 15:31:24 +0200

    Missing toString

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ import data.Student;
 public class Main {
     public static void main(String[] args) {
         // Ezt nyugodtan módosítsd!
-        Person person = new Person();
-        Staff staff = new Staff();
-        Student student = new Student();
+        Person person = new Person("Laci", "123");
+        Staff staff = new Staff("Maki", "234","suli",
+                600);
+        Student student = new Student("Tobi","345",
+                "Futás",1993,38);
     }
 }
